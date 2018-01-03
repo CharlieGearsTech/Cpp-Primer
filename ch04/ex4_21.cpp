94e8684269b35b8c4d277c60c336bc67cddbf6f7
CharlieGearsTech <33494513+CharlieGearsTech@users.noreply.github.com>
2018-01-03 14:55:21 -0600

    Simplify exercise

--- ch04/ex4_21.cpp
+++ ch04/ex4_21.cpp
@@ -1,14 +1,10 @@
 #include <iostream>
 #include <vector>
 
-using std::cout;
-using std::endl;
 using std::vector;
 
 int main()
 {
-    vector<int> ivec{ 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-
     vector<int> v{1,2,3,4,5,6,7,8,9};
 
     for(auto& i:v)
