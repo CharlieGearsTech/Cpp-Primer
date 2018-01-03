0213edfa8099227c296630ba363975246daed50d
CharlieGearsTech <33494513+CharlieGearsTech@users.noreply.github.com>
2018-01-03 14:54:18 -0600

    Change real elements rather than display them
    The exercises require doubling the real container

--- ch04/ex4_21.cpp
+++ ch04/ex4_21.cpp
@@ -9,10 +9,10 @@ int main()
 {
     vector<int> ivec{ 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 
-    for (auto i : ivec) {
-        cout << ((i & 0x1) ? i * 2 : i) << " ";
-    }
-    cout << endl;
+    vector<int> v{1,2,3,4,5,6,7,8,9};
+
+    for(auto& i:v)
+        i=(i&0x1)?i*2:i;
     
     return 0;
 }
