5e01dbd0aa1418bcd6770882576088ea2168f483
CharlieGearsTech <33494513+CharlieGearsTech@users.noreply.github.com>
2018-01-03 14:51:46 -0600

    Doubling the real value rather than displaying it
    I saw that the exercise requires to double the value of the real container, rather that displayed to the user.

--- ch04/ex4_21.cpp
+++ ch04/ex4_21.cpp
@@ -7,12 +7,10 @@ using std::vector;
 
 int main()
 {
-    vector<int> ivec{ 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+    vector<int> v{1,2,3,4,5,6,7,8,9};
 
-    for (auto i : ivec) {
-        cout << ((i & 0x1) ? i * 2 : i) << " ";
-    }
-    cout << endl;
+    for(auto& i:v)
+        i=(i&0x1)?i*2:i;
     
     return 0;
 }
