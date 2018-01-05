c954446c1109013c687352728a79ad544a050032
CharlieGearsTech <33494513+CharlieGearsTech@users.noreply.github.com>
2018-01-05 09:47:30 -0600

    Returning to code format by request
    Change the conditional statement and the braces in the if

--- ch05/ex5_5.cpp
+++ ch05/ex5_5.cpp
@@ -8,12 +8,13 @@ int main()
     vector<string> scores = { "F", "D", "C", "B", "A", "A++" };
     for(int g; cin>>g;){
         string letter;
-        if(g<60)
+        if(g<60){
             letter=scores[0];
+        }
         else{
             letter=scores[(g-50)/10];
             if(g != 100)
-                letter+=(g%10>7)?"+":(g%10<3)?"-":"";
+                letter += g % 10 > 7 ? "+" : g % 10 < 3 ? "-" : "";
         }
         cout<<letter<<endl;
     }}
